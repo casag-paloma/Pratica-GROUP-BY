db918d6dd477ff50102492a161efb945e2eaf2ae
Paloma <casagrande.paloma@gmail.com>
2022-07-28 18:11:56 -0300

    fix question 4 and bonus

--- Prática GROUP BY.sql
+++ Prática GROUP BY.sql
@@ -16,7 +16,23 @@ GROUP BY u.name;
 QUESTAO 4:
 SELECT MAX(j.salary) AS "maximumSalary", r.name AS role
 FROM jobs j
-JOIN roles r ON  r.id = j."roleId"
+JOIN roles r ON  r.id = j."roleId" AND j.active = true
 GROUP BY r.name
 ORDER BY "maximumSalary";
+
+BONUS:
+
+SELECT 
+s.name AS school,
+c.name AS course,
+COUNT(e."userId") AS "studentCount",
+e.status AS role
+FROM educations e
+JOIN schools s ON s.id = e."schoolId"
+JOIN courses c ON c.id = e."courseId"
+WHERE e.status = 'finished' OR e.status = 'ongoing'
+GROUP BY school, course, role
+ORDER BY "studentCount" DESC
+LIMIT 3;
+
 
